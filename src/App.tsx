8aa46022aa339d7f0eac3b2349d57d01fc4561ad
Jozo Skoko <jozo.skoko9@gmail.com>
2022-07-04 02:09:22 +0200

    *Initilising project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Line } from "react-chartjs-2";
 
 function App() {
+  const labels = [1, 2, 3, 4, 5, 6, 7];
+
+  const arpChartData = {
+    labels: labels,
+    datasets: [
+      {
+        label: "APR dataset",
+        data: [33, 53, 85, 41, 44, 65],
+        fill: true,
+        backgroundColor: "rgba(75,192,192,0.2)",
+        borderColor: "rgba(75,192,192,1)",
+        responsive: true,
+      },
+    ],
+  };
+
+  const tlvChartData = {
+    labels: labels,
+    datasets: [
+      {
+        label: "TVL dataset",
+        data: [10, 20, 30, 41, 50, 80],
+        fill: true,
+        backgroundColor: "rgba(75,192,192,0.2)",
+        borderColor: "rgba(75,192,192,1)",
+        responsive: true,
+      },
+    ],
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="chart-wrapper">
+        <Line data={arpChartData} />
+      </div>
+      <div className="chart-wrapper">
+        <Line data={tlvChartData} />
+      </div>
     </div>
   );
 }
